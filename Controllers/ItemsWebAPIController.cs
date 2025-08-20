2e5cdf0c8a27804e10637b736c95d6c9fddd6994
fidrl002 <84311386+fidrl002@users.noreply.github.com>
2025-08-20 19:52:34 +0930

    Create two WebAPI Controllers
    - Adds a scaffolded ItemsWebAPI Controller
    - Adds a scaffolded ParentCategoriesWebAPI Controller
    - Adds a TestAPIs.html page that uses JavaScript fetch to display a list of parent categories and items (screenshot provided)
    - Adds a navbar item to the layout for easy access to the new TestAPIs.html page

---
+++ Controllers/ItemsWebAPIController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAppTest.Data;
+using WebAppTest.Models;
+
+namespace WebAppTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsWebAPIController : ControllerBase
+    {
+        private readonly AmazonOrders2025Context _context;
+
+        public ItemsWebAPIController(AmazonOrders2025Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ItemsWebAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Item>>> GetItems(string? searchText, int? categoryID)
+        {
+            var query = _context.Items
+                .OrderBy(i => i.ItemName)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                query = query
+                    .Where(i => i.ItemName.Contains(searchText));
+            }
+
+            if (categoryID.HasValue)
+            {
+                query = query
+                    .Where(i => i.Category.ParentCategoryId == categoryID);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        // GET: api/ItemsWebAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Item>> GetItem(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        // PUT: api/ItemsWebAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItem(int id, Item item)
+        {
+            if (id != item.ItemId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ItemsWebAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Item>> PostItem(Item item)
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetItem", new { id = item.ItemId }, item);
+        }
+
+        // DELETE: api/ItemsWebAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ItemExists(int id)
+        {
+            return _context.Items.Any(e => e.ItemId == id);
+        }
+    }
+}
